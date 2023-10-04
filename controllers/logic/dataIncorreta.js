b3b369af7082c7e5fd82405f89152cc0782ac88c
Nathan Barros <nathansousa0809@gmail.com>
2023-10-03 23:01:35 -0300

    Mais uma correção da data

--- controllers/logic/dataIncorreta.js
+++ controllers/logic/dataIncorreta.js
@@ -1,7 +1,6 @@
 // Função para validar a data
 function dataIncorreta(data) {
 
-    // Verificando se é data do passado
     const dataAtual = new Date();
 
     const regexData = /^\d{2}\/\d{2}\/\d{4}$/;
@@ -9,17 +8,28 @@ function dataIncorreta(data) {
 
     if (!regexData.test(data)) return true;
 
+    // Converte a data inserida em um objeto Date
+    const dataUsuario = converterData(data);
 
-    const ano = dataAtual.getFullYear();
-    const mes = String(dataAtual.getMonth() + 1).padStart(2, '0'); // O mês começa em 0, então adicionamos 1
-    const dia = String(dataAtual.getDate()).padStart(2, '0');
-
-    const dataFormatada = `${dia}/${mes}/${ano}`
-
-    if (data < dataFormatada) return true;
+    // Verificando se a data é menor que a data atual
+    if (dataUsuario < dataAtual) {
+        return true;
+    }
 
     return false
 
+}
+
+// Função para converter uma data no formato "DD/MM/AAAA" em um objeto Date
+function converterData(dataString) {
+    const partes = dataString.split('/');
+    if (partes.length === 3) {
+        const dia = parseInt(partes[0], 10);
+        const mes = parseInt(partes[1], 10) - 1; // Mês começa em 0
+        const ano = parseInt(partes[2], 10);
+        return new Date(ano, mes, dia);
+    }
+    return null; // Retorna null se o formato for inválido
 }
 
 module.exports = dataIncorreta;
