c804da6ad400bf33a141b130efd3289b2c0ec2a9
Nathan Barros <nathansousa0809@gmail.com>
2023-10-03 22:29:47 -0300

    Uma pequena correção na validação de data

--- controllers/logic/dataIncorreta.js
+++ controllers/logic/dataIncorreta.js
@@ -1,10 +1,16 @@
 // Função para validar a data
-function dataIncorreta (data) {
+function dataIncorreta(data) {
 
     // Verificando se é data do passado
     const dataAtual = new Date();
 
-    if (data < dataAtual) return true;
+    const ano = dataAtual.getFullYear();
+    const mes = String(dataAtual.getMonth() + 1).padStart(2, '0'); // O mês começa em 0, então adicionamos 1
+    const dia = String(dataAtual.getDate()).padStart(2, '0');
+
+    const dataFormatada = `${ano}-${mes}-${dia}`
+
+    if (data < dataFormatada) return true;
 
     return false
 
