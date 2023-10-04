0b750f372a3ae38bbafe96fb8bcae4998d6cf01c
Nathan Barros <nathansousa0809@gmail.com>
2023-10-03 23:59:41 -0300

    Trabalhar com datas é um saco

--- controllers/logic/dataIncorreta.js
+++ controllers/logic/dataIncorreta.js
@@ -12,7 +12,7 @@ function dataIncorreta(data) {
     const dataUsuario = converterData(data);
 
     // Verificando se a data é menor que a data atual
-    if (dataUsuario.getDay() < dataAtual.getDay() && dataUsuario.getMonth() < dataAtual.getMonth() && dataUsuario.getFullYear() < dataAtual.getFullYear()) {
+    if (dataUsuario.getTime() < dataAtual.getTime()) {
         return true;
     }
 
